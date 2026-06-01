d79e5b4d5e01b635aa8728f59ad5d3f822bf4a1b
Helder <hascosta@ufpi.edu.br>
2026-06-01 20:38:40 -0300

    Update and rename firebase-config.example.js to js/script.js

--- firebase-config.example.js
+++
@@ -1,7 +0,0 @@
-
-// Replace with your Firebase project configuration
-export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID"
-};
